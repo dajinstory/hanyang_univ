a7e252939e0b73d2ed6fb20dd368ce7c7dca01ba
Ubuntu <ubuntu@hyeonlhwakim.novalocal>
2019-10-14 17:32:40 +0900

    ...

--- MAT3008_Numerical_Analysis/hw5/main.c
+++ MAT3008_Numerical_Analysis/hw5/main.c
@@ -1,36 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include "matrix_methods.c"
+#include "matrix_functions.c"
 #define ABS(X) (X < 0 ? -X : X)
 #define EPSILON 0.000001
 #define NB 200
 #define N 10000
 
 
-// original function and function to be used with determinating
-typedef void(*funcdPtr)(float,float*,float*);
-
-// two function
-typedef struct funcd{
-	funcdPtr ptr;
-	char* name;
-	float lx;
-	float rx;
-	float xacc;
-}funcd;
-
-// method pointer. for Linear Interpolation, secant, bisection
-typedef float(*method_ptr)(funcdPtr, float, float, float, int*);
-
-// method struct
-typedef struct method{
-	method_ptr ptr;
-	char* name;
-}method;
-
-
 int main(){
-	
+	printf("%f", ABS(-3.0));	
 	int n=N, nb=NB;
 	float xb1[NB], xb2[NB];
 	
